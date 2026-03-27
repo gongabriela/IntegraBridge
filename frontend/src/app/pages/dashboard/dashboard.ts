9c35b4c08b8906fb5926030d91cefcc564d02c10
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-27 13:53:48 +0000

    feat(dashboard): add basic UI structure to dashboard

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class Dashboard {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  async sair() {
+    await this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
