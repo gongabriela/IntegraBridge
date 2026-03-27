4c3ec4ec05076c6b51f8371717b5daf5e4b06384
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-27 11:26:33 +0000

    feat(routing): implement auth guard to protect dashboard and private routes

---
+++ frontend/src/app/guards/auth-guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+
+export const authGuard: CanActivateFn = async (route, state) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const user = await authService.obterUtilizadorAtual();
+
+  if (user) {
+    return true; 
+  } else {
+    router.navigate(['/login']);
+    return false; 
+  }
+};
