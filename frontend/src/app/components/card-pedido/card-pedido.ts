d73e307410c02999b98e02a786b41828e4e4c64c
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-27 15:10:16 +0000

    feat(card-pedido): add card-pedido basic structure to dashboard page

--- frontend/src/app/components/card-pedido/card-pedido.ts
+++ frontend/src/app/components/card-pedido/card-pedido.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPedido } from '../../models/pedido.model'; // O caminho pode variar consoante as tuas pastas
 
 @Component({
   selector: 'app-card-pedido',
-  imports: [],
-  templateUrl: './card-pedido.html',
-  styleUrl: './card-pedido.css',
+  standalone: true,
+  template: `
+    <div class="card-teste">
+      <h4>{{ dados.titulo }}</h4>
+      <p>{{ dados.categoria }}</p>
+      <span>● {{ dados.status }}</span>
+    </div>
+  `,
+  // Podemos adicionar estilos diretamente no componente para testes rápidos
+  styles: [`
+    .card-teste { border: 1px solid #ddd; padding: 20px; border-radius: 15px; background: white; }
+    h4 { margin: 0; }
+    p { color: gray; font-size: 14px; }
+  `]
 })
-export class CardPedido {}
+export class CardPedidoComponent { // <--- Adicionado "Component"
+  @Input({ required: true }) dados!: IPedido; 
+}
