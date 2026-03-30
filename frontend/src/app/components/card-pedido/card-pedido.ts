9e9b72f0550ac39e353cc9a41bdd8514d55fbed9
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-30 11:28:35 +0100

    fix(card-pedido): update component to match pedido interface

--- frontend/src/app/components/card-pedido/card-pedido.ts
+++ frontend/src/app/components/card-pedido/card-pedido.ts
@@ -6,8 +6,8 @@ import { IPedido } from '../../models/pedido.model';
   selector: 'app-card-pedido',
   standalone: true,
   imports: [CommonModule], 
-  templateUrl: './card-pedido.html', // Aponta para o novo ficheiro
-  styleUrl: './card-pedido.css'      // Aponta para o novo ficheiro
+  templateUrl: './card-pedido.html',
+  styleUrl: './card-pedido.css'
 })
 export class CardPedidoComponent {
   @Input({ required: true }) dados!: IPedido;
