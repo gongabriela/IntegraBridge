63c5e413c4a5c9bd6984824d4c72d7ed5696805c
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-30 10:37:34 +0100

    feat(ui): create reusable footer component

--- frontend/src/app/components/footer/footer.ts
+++ frontend/src/app/components/footer/footer.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
 })
-export class Footer {}
+export class FooterComponent {
+  // APAGAR COMENTARIO Pega o ano atual automaticamente do sistema
+  anoAtual = new Date().getFullYear();
+}
