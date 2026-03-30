acb5381246d347a0636c4d94caaf3fc0a1748af2
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-30 11:01:29 +0100

    feat(routes): add main-layout route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { CriarPedido } from './pages/criar-pedido/criar-pedido';
 import { PedidoDetalhe } from './pages/pedido-detalhe/pedido-detalhe';
 import { authGuard } from './guards/auth-guard';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
   { path: 'criar-pedido', component: CriarPedido, canActivate: [authGuard] },
   { path: 'pedido/:id', component: PedidoDetalhe, canActivate: [authGuard] },
-
+  {
+    path: '', // APAGAR COMENTARIO Rota base protegida
+    component: MainLayout, // APAGAR COMENTARIO Carrega o Esqueleto primeiro!
+    canActivate: [authGuard], // APAGAR COMENTARIO Se tiveres o guard de autenticação, fica aqui
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      // APAGAR COMENTARIO No futuro, adicionarás aqui: { path: 'configuracoes', component: ConfigComponent }
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }
 ];
