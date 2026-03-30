f515952decd538d9be36a1f4df2aae3f53ede5e7
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-30 14:38:29 +0100

    feat(jwt-token): enable dynamic jwt token auth to GET requests

--- frontend/src/app/services/pedido.ts
+++ frontend/src/app/services/pedido.ts
@@ -1,23 +1,30 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from, switchMap } from 'rxjs';
 import { IPedido } from '../models/pedido.model';
+import { AuthService } from './auth'; // 1. Importa o teu AuthService
 
 @Injectable({ providedIn: 'root' })
 export class PedidoService {
   private http = inject(HttpClient);
+  private authService = inject(AuthService); // 2. Injeta o serviço de Auth
   
-  // Usa o link do teu Render que está na tua documentação
   private apiUrl = 'https://integrabridge-api.onrender.com/api/pedidos';
 
   obterPedidos(): Observable<IPedido[]> {
-    // Por agora, vamos usar o Token que tens no Postman
-    const token = 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ijg3YWM1YzA5LTdmMDItNGExMS04NWNiLTdjOWMyMWViNTljZSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3NjZm9veHh6Znlsc2R4amtlcmNhLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiJmNWU0MWYxMC0zZWFhLTRkNjQtOWY2Ny01MzZmMmZlMzIzYzMiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzc0ODcxMTAzLCJpYXQiOjE3NzQ4Njc1MDMsImVtYWlsIjoiZ2FicmllbGFAZ2FicmllbGEuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJlbWFpbF92ZXJpZmllZCI6dHJ1ZX0sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoicGFzc3dvcmQiLCJ0aW1lc3RhbXAiOjE3NzQ4Njc1MDN9XSwic2Vzc2lvbl9pZCI6IjQ4ZTQwNDU0LWFkODYtNGM2ZS1hMzZjLTE3MDk1ZTY2ZmMxMyIsImlzX2Fub255bW91cyI6ZmFsc2V9.4cfiLimBHHA3WBf9hJBcIZyll3jn9D1p611v-RrnH1Q-91BSzp2jxsJQt0i9aP7qaIE_-fp7VL88NEFQbaeLww'; 
-    
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
+    // 3. Usamos o cliente supabase que JÁ EXISTE dentro do teu AuthService
+    return from(this.authService.supabase.auth.getSession()).pipe(
+      switchMap(({ data }) => {
+        const token = data.session?.access_token;
+        
+        console.log('Token recuperado para o pedido:', token ? token : 'Não (Vazio)');
 
-    return this.http.get<IPedido[]>(this.apiUrl, { headers });
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+
+        return this.http.get<IPedido[]>(this.apiUrl, { headers });
+      })
+    );
   }
 }
