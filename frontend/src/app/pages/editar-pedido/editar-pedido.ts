319a0dd1e1eb54918e0c7480a3fad5288da6589a
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-31 09:47:27 +0100

    feat: generate editar-pedido components

---
+++ frontend/src/app/pages/editar-pedido/editar-pedido.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-pedido',
+  imports: [],
+  templateUrl: './editar-pedido.html',
+  styleUrl: './editar-pedido.css',
+})
+export class EditarPedido {}
