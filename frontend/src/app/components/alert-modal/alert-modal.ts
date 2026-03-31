4f265ee158d8e2fbcf4b978173882c194ae1a9eb
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-03-31 18:26:05 +0100

    fix: change selector name

--- frontend/src/app/components/alert-modal/alert-modal.ts
+++ frontend/src/app/components/alert-modal/alert-modal.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-modal',
+  selector: 'app-alert-modal',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './alert-modal.html',
